335ed68055c2e9b1a3d96fdc2764650d78fed683
GeorgeSyarov <106813705+GeorgeSyarov@users.noreply.github.com>
2023-10-12 18:54:02 +0300

    solve simple task

---
+++ Function-Exercise/matrix-n.js
@@ -0,0 +1,10 @@
+function matrix (n){
+    for(let i = 1;i<= n;i++){
+        let temp = [];
+        for(let j = 1; j <= n;j++){
+            temp.push(n);
+        }
+        console.log(temp.join(' '));
+    }
+}
+matrix(7);
